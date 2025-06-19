4231a70b17b81c4151f8ed937c6f9b6e6fb1b8ad
Gargi A. Pratapwar <gargipratapwar@gmail.com>
2025-06-19 22:06:09 +0530

    connection done ta daaaa

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,6 @@
-//main express app
+const mongoose = require('mongoose');
 
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+mongoose.connect('mongodb+srv://gargipratapwar:ZnJP8ql1kEiGy1ql@cluster0.58ztyt4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('✅ MongoDB Connected Successfully'))
+  .catch(err => console.error('❌ Connection error:', err));
 
-// Load environment variables
-dotenv.config();
-
-// Initialize app
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("API is running ✅");
-});
-
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected ✅"))
-  .catch((err) => console.error("MongoDB connection failed ❌", err));
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
