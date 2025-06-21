2549653c4df3e7a4f541b9d4bae273083f015a1c
Gargi A. Pratapwar <gargipratapwar@gmail.com>
2025-06-21 20:03:10 +0530

    Connected MongoDB and updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+require('dotenv').config(); // Make sure this is at the top
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gargipratapwar:ZnJP8ql1kEiGy1ql@cluster0.58ztyt4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => console.log('✅ MongoDB Connected Successfully'))
-  .catch(err => console.error('❌ Connection error:', err));
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ MongoDB Connected Successfully'))
+.catch(err => console.error('❌ Connection error:', err));
